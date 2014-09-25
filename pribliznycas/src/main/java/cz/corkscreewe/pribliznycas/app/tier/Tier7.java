0a2faf0b5aaca6fe13b3603325668871bd701da3
Pavel Vanecek <pavel.vanecek@merck.com>
2014-09-25 10:25:22 +0200

    more grammar fixes

--- pribliznycas/src/main/java/cz/corkscreewe/pribliznycas/app/tier/Tier7.java
+++ pribliznycas/src/main/java/cz/corkscreewe/pribliznycas/app/tier/Tier7.java
@@ -12,8 +12,37 @@ import cz.corkscreewe.pribliznycas.app.helpers.MyTranslator;
  */
 public class Tier7 implements IDoubleTier {
 
-    int[] hours_str = Tier5.HOURS;
-    int[] half_hours = {
+    protected static final int[] hours_str = Tier5.HOURS;
+
+    protected static final int[] quarter_hours = {
+            R.string.midnight,
+            R.string.quarter_to_one,
+            R.string.hours_two,
+            R.string.hours_three,
+            R.string.hours_four,
+            R.string.hours_five,
+            R.string.hours_six,
+            R.string.hours_seven,
+            R.string.hours_eight,
+            R.string.hours_nine,
+            R.string.hours_ten,
+            R.string.hours_eleven,
+            R.string.hours_noon,
+            R.string.quarter_to_one,
+            R.string.hours_two,
+            R.string.hours_three,
+            R.string.hours_four,
+            R.string.hours_five,
+            R.string.hours_six,
+            R.string.hours_seven,
+            R.string.hours_eight,
+            R.string.hours_nine,
+            R.string.hours_ten,
+            R.string.hours_eleven,
+            R.string.midnight
+    };
+
+    protected static final int[] half_hours = {
             R.string.half_to_twelve,
             R.string.half_to_one,
             R.string.half_to_two,
@@ -46,21 +75,30 @@ public class Tier7 implements IDoubleTier {
         int hours = c.get(Calendar.HOUR_OF_DAY); // 0 - indexed
         int minutes = c.get(Calendar.MINUTE);
         String[] doubleApproxTime = new String[2];
+        int transcriptedHours;
         if (minutes <= 8) {
-            doubleApproxTime[0] = MyTranslator.getHoursItWas(res, hours);
-            doubleApproxTime[1] = res.getString(hours_str[hours]);
+            // "ctvrt na dvanact" versus "bude pulnoc"
+            transcriptedHours = hours;
+            doubleApproxTime[0] = MyTranslator.getHoursItWas(res, transcriptedHours);
+            doubleApproxTime[1] = res.getString(hours_str[transcriptedHours]);
         } else if (minutes <= 21) {
+            // "ctvrt na dvanact" versus "bude pulnoc"
+            transcriptedHours = (hours % 12) + 1;
             doubleApproxTime[0] = res.getString(R.string.quarter_);
-            doubleApproxTime[1] = res.getString(R.string.to__) + res.getString(hours_str[hours + 1]);
+            doubleApproxTime[1] = res.getString(R.string.to__) + res.getString(quarter_hours[transcriptedHours]);
         } else if (minutes <= 40) {
             doubleApproxTime[0] = res.getString(R.string.half_to);
             doubleApproxTime[1] = res.getString(half_hours[hours + 1]);
         } else if (minutes <= 50) {
+            // "ctvrt na dvanact" versus "bude pulnoc"
+            transcriptedHours = (hours % 12) + 1;
             doubleApproxTime[0] = res.getString(R.string.three_quarters_);
-            doubleApproxTime[1] = res.getString(R.string.to__) + res.getString(hours_str[hours + 1]);
+            doubleApproxTime[1] = res.getString(R.string.to__) + res.getString(quarter_hours[transcriptedHours]);
         } else /* if (minutes > 51 && minutes < 8) */ {
-            doubleApproxTime[0] = MyTranslator.getHoursSoon(res, hours + 1);
-            doubleApproxTime[1] = res.getString(hours_str[hours + 1]);
+            // "ctvrt na dvanact" versus "bude pulnoc"
+            transcriptedHours = hours + 1;
+            doubleApproxTime[0] = MyTranslator.getHoursSoon(res, transcriptedHours);
+            doubleApproxTime[1] = res.getString(hours_str[transcriptedHours]);
         }
         return doubleApproxTime;
     }
