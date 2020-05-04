53ec5869f2007817506df1fa1cd0a1de583567d0
Pavel Vanecek <corkscreewe+github@gmail.com>
2020-05-04 20:49:14 +1000

    Cleanup, fixing double widget buttons

--- pribliznycas/src/main/java/cz/corkscreewe/pribliznycas/app/widget/TimeWidget.java
+++ pribliznycas/src/main/java/cz/corkscreewe/pribliznycas/app/widget/TimeWidget.java
@@ -17,10 +17,12 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.util.Arrays;
 import java.util.Calendar;
 
 import cz.corkscreewe.pribliznycas.app.R;
 
+import static android.app.PendingIntent.FLAG_IMMUTABLE;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE;
 import static android.content.Context.MODE_PRIVATE;
@@ -46,7 +48,7 @@ public abstract class TimeWidget extends AppWidgetProvider {
     private static final String ACTION_TIER_CHANGE_CLICK_UP = "cz.corkscreewe.pribliznycas.TIER_CHANGE_CLICK_ACTION_UP";
     private static final String ACTION_TIER_CHANGE_CLICK_DOWN = "cz.corkscreewe.pribliznycas.TIER_CHANGE_CLICK_ACTION_DOWN";
     private static final String ACTIVE_TIERS_PREFERENCE = "cz.corkscreewe.pribliznycas.ACTIVE_TIERS_PREFERENCE";
-    private final int ALARM_ID = 1;
+    private final int ALARM_INTENT_ID = 1;
 
     @Override
     public void onUpdate(@NonNull Context context, AppWidgetManager appWidgetManager, @NonNull int[] appWidgetIds) {
@@ -59,18 +61,21 @@ public abstract class TimeWidget extends AppWidgetProvider {
     }
 
     private void registerAlarmIntent(@NonNull Context context) {
-        Context applicationContext = context;//.getApplicationContext();
-        AlarmManager alarmManager = (AlarmManager) applicationContext.getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pendingIntent = getPendingIntent(applicationContext);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        d("registering alarm");
+        PendingIntent pendingIntent = getPendingIntent(context);
         Calendar calendar = Calendar.getInstance();
-        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), INTERVAL_MILLIS, pendingIntent);
-        Log.d("widget", "registering alarm");
+        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis() + INTERVAL_MILLIS, INTERVAL_MILLIS, pendingIntent);
     }
 
     private PendingIntent getPendingIntent(@NonNull Context context) {
         Intent intent = new Intent(context, getClass());
         intent.setAction(ALARM_TICK);
-        return PendingIntent.getBroadcast(context, ALARM_ID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return PendingIntent.getBroadcast(context, ALARM_INTENT_ID, intent, FLAG_IMMUTABLE);
+        } else {
+            return PendingIntent.getBroadcast(context, ALARM_INTENT_ID, intent, FLAG_UPDATE_CURRENT);
+        }
     }
 
     private void cancelAlarmIntent(@NonNull Context context) {
@@ -80,29 +85,30 @@ public abstract class TimeWidget extends AppWidgetProvider {
 
     private void registerReceiver(@NonNull Context context) {
         IntentFilter intentFilter = new IntentFilter();
+        // For some reason, ACTION_TIME_TICK is not received reliably - so Alarm it is.
 //        intentFilter.addAction(Intent.ACTION_TIME_TICK);
-//        intentFilter.addAction(ALARM_TICK);
         intentFilter.addAction(Intent.ACTION_TIME_CHANGED);
         intentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
-        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
         Context applicationContext = context.getApplicationContext();
         if (applicationContext != null) {
-            Log.d("widget", "using application context");
+            d("using application context");
             applicationContext.registerReceiver(this, intentFilter);
         } else {
             Log.wtf("applicationContext", "is null and should not be null");
         }
-        Log.d("widget", "registering receiver");
+        d("registering receiver");
     }
 
     private void unregisterReceiver(@NonNull Context context) {
-        context.getApplicationContext().unregisterReceiver(this);
+        try {
+            context.getApplicationContext().unregisterReceiver(this);
+        } catch (IllegalArgumentException e) {
+            // It may happen that the receiver is already unregistered by system; in that case do nothing.
+        }
     }
 
     @NonNull
     private RemoteViews setupWidget(@NonNull Context applicationContext, @NonNull AppWidgetManager appWidgetManager, int appWidgetId) {
-        // it might happen that the widget is running on keyguard screen
         boolean isKeyguard = isKeyguard(appWidgetManager, appWidgetId);
         if (isKeyguard) {
             return setupKeyguardWidgetIntents(applicationContext, appWidgetId);
@@ -118,28 +124,36 @@ public abstract class TimeWidget extends AppWidgetProvider {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) { // 17
             Bundle appWidgetOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
             int category = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY);
-            if (category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD) {
-                isKeyguard = true;
-            }
+            return category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;
         }
-        Log.d("isKeyguard", String.valueOf(isKeyguard));
-        return isKeyguard;
+        return false;
     }
 
     @NonNull
     private RemoteViews setupWidgetBase(@NonNull Context applicationContext, int appWidgetId, int templateId) {
-        Log.d("widget", "setupWidgetBase");
+        d("setupWidgetBase", appWidgetId);
+
         RemoteViews remoteViews = new RemoteViews(applicationContext.getPackageName(), templateId);
 
         Intent nextIntent = new Intent(applicationContext, getClass());
         nextIntent.setAction(ACTION_TIER_CHANGE_CLICK_UP);
         nextIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        PendingIntent nextPendingIntent = PendingIntent.getBroadcast(applicationContext, appWidgetId, nextIntent, FLAG_UPDATE_CURRENT);
+        PendingIntent nextPendingIntent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            nextPendingIntent = PendingIntent.getBroadcast(applicationContext, appWidgetId, nextIntent, FLAG_IMMUTABLE);
+        } else {
+            nextPendingIntent = PendingIntent.getBroadcast(applicationContext, appWidgetId, nextIntent, FLAG_UPDATE_CURRENT);
+        }
 
         Intent prevIntent = new Intent(applicationContext, getClass());
         prevIntent.setAction(ACTION_TIER_CHANGE_CLICK_DOWN);
         prevIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        PendingIntent prevPendingIntent = PendingIntent.getBroadcast(applicationContext, appWidgetId, prevIntent, FLAG_UPDATE_CURRENT);
+        PendingIntent prevPendingIntent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            prevPendingIntent = PendingIntent.getBroadcast(applicationContext, appWidgetId, prevIntent, FLAG_IMMUTABLE);
+        } else {
+            prevPendingIntent = PendingIntent.getBroadcast(applicationContext, appWidgetId, prevIntent, FLAG_UPDATE_CURRENT);
+        }
 
         remoteViews.setOnClickPendingIntent(R.id.button_next, nextPendingIntent);
         remoteViews.setOnClickPendingIntent(R.id.button_prev, prevPendingIntent);
@@ -162,30 +176,43 @@ public abstract class TimeWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        Log.d("widget", "onEnabled");
+        d("onEnabled");
         registerReceiver(context);
         registerAlarmIntent(context);
     }
 
     @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        d("onDeleted", appWidgetIds);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(ACTIVE_TIERS_PREFERENCE, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            editor.remove(String.valueOf(i));
+        }
+        editor.apply();
+    }
+
+    @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
-        Log.d("widget", "onDisabled");
-
         AppWidgetManager manager = AppWidgetManager.getInstance(context.getApplicationContext());
         int[] appWidgetIds = getAppWidgetIds(context.getApplicationContext(), manager);
+
+        d("onDisabled", appWidgetIds);
 
         // last widget has been removed, cancel the alarmManager
         if (appWidgetIds.length == 0) {
             unregisterReceiver(context);
             cancelAlarmIntent(context);
+            cleanupPreferences(context);
         }
     }
 
     @Override
     public void onReceive(@NonNull Context context, @NonNull Intent intent) {
         String action = intent.getAction();
-        Log.d("widget", "intent received: " + action);
+        d("intent received: " + action);
         if (action == null) {
             return;
         }
@@ -212,7 +239,7 @@ public abstract class TimeWidget extends AppWidgetProvider {
                 Bundle extras = intent.getExtras();
                 if (extras != null) {
                     int intentWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-                    d("ACTION_APPWIDGET_UPDATE: " + intentWidgetId);
+                    d("ACTION_APPWIDGET_UPDATE", intentWidgetId);
                     if (intentWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
                         updateAllWidgets(context, manager);
                     } else {
@@ -221,16 +248,26 @@ public abstract class TimeWidget extends AppWidgetProvider {
                 }
                 break;
             }
-            default: {
+            case ALARM_TICK:
+                // case Intent.ACTION_TIME_TICK:
+            case Intent.ACTION_TIME_CHANGED:
+            case Intent.ACTION_TIMEZONE_CHANGED: {
                 updateAllWidgets(context, manager);
+                break;
             }
         }
+
+        // Now. Android sometimes forgets Alarms for reasons. Some state is left where it should not,
+        // and my alarms are lost. It is safe to just call register again all the time,
+        // because the system will just reuse the same PendingIntent if one is already registered
+        // (thanks to the FLAG_IMMUTABLE) and so we're done here.
+        registerAlarmIntent(context);
         super.onReceive(context, intent);
     }
 
     private void updateAllWidgets(@NonNull Context context, AppWidgetManager manager) {
-        Log.d("widget", "updating all widgets");
         int[] appWidgetIds = getAppWidgetIds(context, manager);
+        d("updating all widgets ", appWidgetIds);
         for (int appWidgetId : appWidgetIds) {
             int activeTier = getActiveTier(context, appWidgetId);
             updateWidget(context, manager, appWidgetId, activeTier);
@@ -262,12 +299,10 @@ public abstract class TimeWidget extends AppWidgetProvider {
         int min = 0;
         int max = getMaxTier();
         int tier = Math.min(max, Math.max(min, sharedPreferences.getInt(String.valueOf(appWidgetId), getDefaultTier())));
-//        d("active tier: " + tier);
         return tier;
     }
 
     private void updateTier(@NonNull Context context, int appWidgetId, int delta, @NonNull AppWidgetManager manager) {
-//        d("updateTier: " + delta);
         SharedPreferences sharedPreferences = context.getSharedPreferences(ACTIVE_TIERS_PREFERENCE, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         int maxTier = getMaxTier();
@@ -279,8 +314,21 @@ public abstract class TimeWidget extends AppWidgetProvider {
         }
     }
 
+    private void cleanupPreferences(@NonNull Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(ACTIVE_TIERS_PREFERENCE, MODE_PRIVATE);
+        sharedPreferences.edit().clear().apply();
+    }
+
     private void d(String msg) {
         Log.d(getClass().getName(), msg);
+    }
+
+    private void d(String msg, int i) {
+        d(msg + ": " + i);
+    }
+
+    private void d(String msg, int[] arr) {
+        d(msg + ": " + Arrays.toString(arr));
     }
 
 }
